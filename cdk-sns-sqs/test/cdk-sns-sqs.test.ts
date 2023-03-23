d02e1da0d820ea45d6448a48fcb51c46fdcf5eca
Tony Chou <cool21540125@gmail.com>
2023-03-23 18:12:13 +0800

    init

---
+++ cdk-sns-sqs/test/cdk-sns-sqs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkSnsSqs from '../lib/cdk-sns-sqs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-sns-sqs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkSnsSqs.CdkSnsSqsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
