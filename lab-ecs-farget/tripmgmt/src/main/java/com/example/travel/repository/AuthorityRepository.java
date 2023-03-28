fda9e3c82f25c34d51985dead2a62e26772351ec
TonyChou <cool21540125@gmail.com>
2023-03-28 23:17:48 +0800

    Initial version of tripmgmt generated by generator-jhipster@7.1.0

---
+++ lab-ecs-farget/tripmgmt/src/main/java/com/example/travel/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.example.travel.repository;
+
+import com.example.travel.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ lab-ecs-farget/tripmgmt/src/test/java/com/example/travel/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.example.travel;
+
+import com.example.travel.TripmgmtApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = TripmgmtApp.class)
+public @interface IntegrationTest {
+}
