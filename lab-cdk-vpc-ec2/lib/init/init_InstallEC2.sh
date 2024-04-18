1989fbb7727f460d709b5ea47680222b137e1049
Tony Chou <cool21540125@gmail.com>
2024-04-18 17:22:01 +0800

    cloudformation - user data for EC2

--- lab-cdk-vpc-ec2/lib/init/init_InstallEC2.sh
+++ lab-cdk-vpc-ec2/lib/init/init_InstallEC2.sh
@@ -3,26 +3,27 @@
 # yum update -y
 # yum groupinstall -y "Development Tools"
 
-
 ### SSM Agent
 yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
 systemctl start amazon-ssm-agent
 systemctl enable amazon-ssm-agent || true
 
-
-### Nginx
-amazon-linux-extras install -y nginx1
-systemctl start nginx
-systemctl enable nginx
-
-
 ### CloudWatch Unified Agent
 yum install -y amazon-cloudwatch-agent
-systemctl start  amazon-cloudwatch-agent
-systemctl enable  amazon-cloudwatch-agent
+systemctl start amazon-cloudwatch-agent
+systemctl enable amazon-cloudwatch-agent
 
+### X-Ray Agent
+# https://docs.aws.amazon.com/xray/latest/devguide/xray-daemon-ec2.html
+# curl https://s3.us-east-2.amazonaws.com/aws-xray-assets.us-east-2/xray-daemon/aws-xray-daemon-3.x.rpm -o /home/ec2-user/xray.rpm
+# yum install -y /home/ec2-user/xray.rpm
 
 # # Code Deploy Agent
 # wget https://aws-codedeploy-us-west-2.s3.us-west-2.amazonaws.com/latest/install
 # chmod +x ./install
 # ./install auto
+
+### Nginx
+amazon-linux-extras install -y nginx1
+systemctl start nginx
+systemctl enable nginx
